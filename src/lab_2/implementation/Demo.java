f3b59666ab75887d7408a8b60c28bb080626c7f5
Pavlo Hrytsyshyn <phrytsyshyn@ua.imshealth.com>
2023-04-01 19:47:47 +0300

    added lab2 Demo output

--- src/lab_2/implementation/Demo.java
+++ src/lab_2/implementation/Demo.java
@@ -10,5 +10,10 @@ public class Demo {
                 .setMaxSpeed(300)
                 .build();
         System.out.println(customBike.toString());
+
+        /*
+            bike - Bike{numWheels=2, color='blue', maxSpeed=100}
+            customBike - Bike{numWheels=2, color='red', maxSpeed=300}
+         */
     }
 }
